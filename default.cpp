095568a5fbac12d2d3640ffb8c317149673d9b30
Vinoth Energetic <vinothcse2000@gmail.com>
2020-12-24 10:33:44 +0530

    Update and rename main.cpp to default.cpp

---
+++ default.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+int main()
+{	
+	using namespace std;
+
+
+    	cout<< "HELLO" << '\n';
+	
+	return 0;
+}
