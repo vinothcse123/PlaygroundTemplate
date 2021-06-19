b29b4670979cb48d327ded353a55790a8a009e19
Vinoth Energetic <vinothcse2000@gmail.com>
2021-06-19 09:27:58 +0530

    Update default.cpp

--- default.cpp
+++ default.cpp
@@ -6,10 +6,16 @@ using std::vector;
 using std::cout;
 using std::string;
 
+void demoFunction1()
+{
+
+}
+
 int main()
 {
     int myNumber=-1;
 
+    demoFunction1();
 
     cout<< "Result :" << myNumber << '\n';	
     return 0;
