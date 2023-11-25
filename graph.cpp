3a70a8927d773ae74fd8444d3976dc53d723d24c
Vinoth Energetic <vinothcse2000@gmail.com>
2023-11-25 15:59:46 +0530

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <list>
+
+
+class Graph
+{
+    public:
+    int data=0;
+    std::vector<std::list<int>> adj;
+
+    Graph(int size)
+    {
+        adj.resize(size);
+    }
+
+    void addEdge(int left,int right)
+    {
+        adj[left].push_back(right);
+    }
+};
+
+
+int main()
+{
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(2,3);
+    g.addEdge(1,5);
+    g.addEdge(3,5);
+}
