e8f92efcd8674faf51e6185e9e890bed6a5eaca7
Vinoth Energetic <vinothcse2000@gmail.com>
2024-01-18 17:25:46 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -28,7 +28,7 @@ int main()
 
     g.addEdge(0,1);
     g.addEdge(0,2);
-    g.addEdge(2,3);
-    g.addEdge(1,5);
-    g.addEdge(3,5);
+    g.addEdge(1,3);
+    g.addEdge(1,4);
+    g.addEdge(2,5);
 }
